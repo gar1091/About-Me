1dca490a6370b810f8ecad9d428500d78e3c2f7a
Gar1091 <gar1091@icloud.com>
2022-11-08 19:05:58 -0500

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Garrett Neher on 11/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
